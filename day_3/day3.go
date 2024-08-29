18785fc9e486a64c64d513a40e63f1eba2e543fe
nungo <nued65@gmail.com>
2024-08-29 17:45:05 +0300

    fix:fix the wrong conversion of int to str that caused a wrong output in the other way of doing same solution

--- day_3/day3.go
+++ day_3/day3.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -14,53 +15,58 @@ func main() {
 func Direct(s string) int {
 	a, b := 0, 0
 
-	// points := make(map[int]string)
-	// for i, sign := range s {
-	// 	if sign == '>' || sign == '<' {
-	// 		if sign == '>' {
-	// 			b += 1
-	// 		} else {
-	// 			b -= 1
-	// 		}
-	// 		// points = append(points, []int{b, a})
-	// 		points[i] = string(b) + string(a)
-	// 	} else if sign == '^' || sign == 'v' {
-	// 		if sign == '^' {
-	// 			a += 1
-	// 		} else {
-	// 			a -= 1
-	// 		}
-	// 		points[i] = string(b) + string(a)
-	// 	}
-	// }
-	// // adding the origin which originally wasnt in the map or accounted for
-	// points[len(points)] = "00"
+	points := make(map[int]string)
+	for i, sign := range s {
+		if sign == '>' || sign == '<' {
+			if sign == '>' {
+				b += 1
+			} else {
+				b -= 1
+			}
 
-	// // copying a map
-	// points1 := make(map[int]string)
-	// for i, v := range points {
-	// 	points1[i] = v
-	// }
+			b1 := strconv.Itoa(b)
+			a1 := strconv.Itoa(a)
+			points[i] = string(b1) + string(a1)
 
-	// for _, t := range points {
-	// 	c := 0
-	// 	for i, j := range points1 {
-	// 		if t == j {
-	// 			if c == 0 {
-	// 				c += 1
-	// 			} else {
-	// 				delete(points1, i)
-	// 			}
-	// 		}
-	// 	}
-	// 	if c > 1 {
-	// 		b += 1
-	// 	}
-	// }
+		} else if sign == '^' || sign == 'v' {
+			if sign == '^' {
+				a += 1
+			} else {
+				a -= 1
+			}
+			b2 := strconv.Itoa(b)
+			a2 := strconv.Itoa(a)
+			points[i] = b2 + a2
+		}
+	}
+	// adding the origin which originally wasnt in the map or accounted for
+	points[len(points)] = "00"
 
-	// return len(points)
+	// copying a map
+	points1 := make(map[int]string)
+	for i, v := range points {
+		points1[i] = v
+	}
+
+	for _, t := range points {
+		c := 0
+		for i, j := range points1 {
+			if t == j {
+				if c == 0 {
+					c += 1
+				} else {
+					delete(points1, i)
+				}
+			}
+		}
+		if c > 1 {
+			b += 1
+		}
+	}
+
+	return len(points1)
 	// alternative way simpler since keys are unique in maps duplicates are handled automatically
-	points := make(map[[2]int]bool)
+	/*points := make(map[[2]int]bool)
 	points[[2]int{a, b}] = true
 
 	for _, cha := range s {
@@ -76,5 +82,5 @@ func Direct(s string) int {
 		}
 		points[[2]int{a, b}] = true
 	}
-	return len(points)
+	return len(points)*/
 }
